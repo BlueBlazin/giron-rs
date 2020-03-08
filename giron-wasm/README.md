0b688cb85679a867b71a86f844c371c1d9b135ff
Sanjeet N. Dasharath <sanjeetdasharath@gmail.com>
2020-03-08 09:57:34 +0530

    update readme

--- giron-wasm/README.md
+++ giron-wasm/README.md
@@ -1,3 +1,17 @@
 # giron-wasm
 
-A WebAssembly based ECMAScript parser that outputs an Abstract Syntax Tree (AST) JSON in the ESTree format.
+A WebAssembly module for parsing Javascript.
+
+## Usage
+
+The easiest way to see how you can use `giron-wasm` is with the wasm-app template.
+
+Just run `npm init wasm-app` and this will create a boilerplate wasm project for you.
+
+Now you can install `giron-wasm` and import it like any normal npm package.
+
+## API
+
+`function parseScript(source: string)` - Parses a string as a javascript script.
+
+`function parseModule(source: string)` - Parses a string as a javascript module. Modules in javascript are parsed as strict mode code and allow import/export declarations.
--- giron-wasm/src/lib.rs
+++ giron-wasm/src/lib.rs
@@ -11,12 +11,16 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(js_name = parseScript)]
 pub fn parse_script(source: String) -> JsValue {
-    let ast = giron::parse_script(source).unwrap();
-    JsValue::from_serde(&ast).unwrap()
+    match giron::parse_script(source) {
+        Ok(ast) => JsValue::from_serde(&ast).unwrap(),
+        Err(err) => JsValue::from(format!("{:?}", err)),
+    }
 }
 
 #[wasm_bindgen(js_name = parseModule)]
 pub fn parse_module(source: String) -> JsValue {
-    let ast = giron::parse_module(source).unwrap();
-    JsValue::from_serde(&ast).unwrap()
+    match giron::parse_module(source) {
+        Ok(ast) => JsValue::from_serde(&ast).unwrap(),
+        Err(err) => JsValue::from(format!("{:?}", err)),
+    }
 }
